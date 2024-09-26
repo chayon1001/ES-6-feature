64661d6f53a42506b62cdae65f6ce6d4999d2453
chayon1001 <baust.cse.200201001@gmail.com>
2024-09-26 23:25:39 +0600

    added the object key, values, entries, delete, freeze

---
+++ key-value-entries-delete.js
@@ -0,0 +1,12 @@
+const friends = {
+    name : 'harishankar barman',
+    age : 25,
+    isMarried : true
+}
+
+// const res = Object.keys(friends);
+// const res = Object.values(friends);
+
+const res = Object.entries(friends)
+
+console.log(res);
